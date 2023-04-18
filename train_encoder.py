bcf20c6f9bbdb621fbee615834c93b9294c6bdf1
Pietro Talli <pietro.talli@studenti.unipd.it>
2023-04-18 15:48:27 +0200

    train_encoder complete

--- train_encoder.py
+++ train_encoder.py
@@ -18,7 +18,6 @@ parser.add_argument('--embedding_dim', type=int, help='The embedding dimension o
 parser.add_argument('--num_codewords', type=int, help='The number of codewords to use i the quantizer', required=False)
 parser.add_argument('--retrain', type=bool, help='Set to False if the encoder-decoder already exist', required=False)
 
-
 args = parser.parse_args()
 
 num_samples = 50000 #number of tuples to collect
@@ -31,8 +30,6 @@ num_training_updates = 100
 embedding_dim = 64
 num_embeddings = 64
 retrain = True
-
-print(args)
 
 if args.epochs:
     num_training_updates = args.epochs
@@ -61,15 +58,18 @@ decoder = Decoder(embedding_dim, num_hiddens, num_residual_layers, num_residual_
 # Collect a dataset of tuples (o_t, o_{t+1})
 
 if collect_dataset:
-    create_dataset(num_samples, 'dataset')
+    if not os.path.exists('../dataset'):
+        os.mkdir('../dataset')
+        os.mkdir('../dataset/images')
+    print('Collecting the dataset')
+    create_dataset(num_samples, '../dataset')
+    print('Dataset collected')
 
-writer = SummaryWriter()
+writer = SummaryWriter('../runs')
 #Parameters of the VQ-VAE
 
 # if gpu is to be used
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-print('using ' + device.type)
-
 
 param_to_optimize = [
                 {'params': encoder.parameters()},
@@ -81,18 +81,23 @@ quantizer.train()
 decoder.train()
 
 if not retrain:
-    param_to_optimize = [{'params': quantizer.parameters()}]
-    encoder.load_state_dict(torch.load('models/encoder.pt'))
-    decoder.load_state_dict(torch.load('models/decoder.pt'))
-    encoder.eval()
-    decoder.eval()
+    if os.path.exists('../models'):
+        param_to_optimize = [{'params': quantizer.parameters()}]
+        encoder.load_state_dict(torch.load('../models/encoder.pt'))
+        decoder.load_state_dict(torch.load('../models/decoder.pt'))
+        encoder.eval()
+        decoder.eval()
+#Create the models folder
+if not os.path.exists('../models'):
+    os.mkdir('../models')
+
 
 learning_rate = 1e-3
 optimizer = torch.optim.Adam(param_to_optimize, lr=learning_rate, amsgrad=False)
 batch_size = 128
 
 # Create a dataset
-dataset = FramesDataset('dataset/description.csv', 'dataset/images', ToTensor())
+dataset = FramesDataset('../dataset/description.csv', '../dataset/images', ToTensor())
 dataloader = DataLoader(dataset, batch_size=128,
                         shuffle=True, num_workers=6)
 
@@ -103,6 +108,11 @@ train_res_perplexity = []
 
 reset = True
 time_step = 0 
+
+print('using ' + device.type)
+encoder.to(device)
+quantizer.to(device)
+decoder.to(device)
 
 for i in range(num_training_updates):
     for i_batch, sample_batched in enumerate(dataloader):
@@ -123,7 +133,7 @@ for i in range(num_training_updates):
         train_res_perplexity.append(perplexity.item())
         reset = False
         
-        if (i_batch+1) % 50 == 0:
+        if (i_batch+1) % 100 == 0:
             print('%d iterations' % (i+1))
             print('recon_error: %.5f' % np.mean(train_res_recon_error[-100:]))
             print('perplexity: %.5f' % np.mean(train_res_perplexity[-100:]))
@@ -133,10 +143,10 @@ for i in range(num_training_updates):
             writer.add_scalar('Loss/train', np.mean(train_res_recon_error[-100:]), time_step)
             writer.add_scalar('Perplexity/batch', np.mean(train_res_perplexity[-100:]), time_step)
             time_step += 1
-            torch.save(encoder.state_dict(), 'models/encoder_training.pt')
-            torch.save(quantizer.state_dict(), 'models/quantizer_'+str(num_embeddings)+'_training.pt')
-            torch.save(decoder.state_dict(), 'models/decoder_training.pt')
+            torch.save(encoder.state_dict(), '../models/encoder_training.pt')
+            torch.save(quantizer.state_dict(), '../models/quantizer_'+str(num_embeddings)+'_training.pt')
+            torch.save(decoder.state_dict(), '../models/decoder_training.pt')
 
-torch.save(encoder.state_dict(), 'models/encoder.pt')
-torch.save(quantizer.state_dict(), 'models/quantizer_'+str(num_embeddings)+'.pt')
-torch.save(decoder.state_dict(), 'models/decoder.pt')
+torch.save(encoder.state_dict(), '../models/encoder.pt')
+torch.save(quantizer.state_dict(), '../models/quantizer_'+str(num_embeddings)+'.pt')
+torch.save(decoder.state_dict(), '../models/decoder.pt')

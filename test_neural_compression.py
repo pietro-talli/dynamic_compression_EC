b60f3cd7037ff989c68c895b4fc4c2b31380e1f7
Pietro Talli <pietro.talli@studenti.unipd.it>
2024-01-19 16:57:14 +0100

    add compress ai

--- test_neural_compression.py
+++ test_neural_compression.py
@@ -10,21 +10,33 @@ import torchvision.transforms
 from PIL import Image
 import urllib.request
 
+import compressai 
+comp_ai_model = compressai.zoo.cheng2020_attn(quality=1, pretrained=True)
+comp_ai_model.eval()
+
+
+
+
+
 x = Image.open("../dataset/images/0_0.png")
 
 transform = torchvision.transforms.Compose([
     torchvision.transforms.PILToTensor(),
-    torchvision.transforms.Resize((16, 32)),
+    torchvision.transforms.Resize((64, 128)),
+    # convert from RGB to grayscale
+    #torchvision.transforms.Grayscale(num_output_channels=1),
     torchvision.transforms.ConvertImageDtype(torch.float32),
 ])
 
 x = transform(x).unsqueeze(0)
 
-matplotlib.pyplot.figure(figsize=(12, 18))
 
-matplotlib.pyplot.imshow(x.squeeze().permute(1, 2, 0))
+#matplotlib.pyplot.figure(figsize=(12, 18))
 
-matplotlib.pyplot.show()
+#print(x.shape)
+#matplotlib.pyplot.imshow(x.squeeze().permute(1, 2, 0))
+
+#matplotlib.pyplot.show()
 
 network = FactorizedPriorAutoencoder()
 
@@ -33,20 +45,24 @@ url = "https://dl.fbaipublicfiles.com/neuralcompression/models/factorized_prior_
 state_dict = torch.hub.load_state_dict_from_url(url)
 
 network.load_state_dict(state_dict, strict=False)
-print('bottleneck', network.bottleneck._quantized_cdf) 
 
 with torch.no_grad():
     strings, broadcast_size = network.compress(x)
-    print(type(strings[0][0]))
-    print(len(strings[0][0])*8)
-    print(broadcast_size)
+    #print(type(strings[0][0]))
+    #print(len(strings[0][0])*8)
+    #print(broadcast_size)
 
 with torch.no_grad():
     x_hat = network.decompress(strings, broadcast_size)
 
+with torch.no_grad():
+    z = comp_ai_model.compress(x)
+    print(len(z['strings'][0][0]))
+    x_hat = comp_ai_model.decompress(z['strings'], z['shape'])
+
 matplotlib.pyplot.figure(figsize=(12, 18))
 
-matplotlib.pyplot.imshow(x_hat.squeeze().permute(1, 2, 0))
+matplotlib.pyplot.imshow(x_hat['x_hat'].squeeze().permute(1, 2, 0))
 
 matplotlib.pyplot.show()
 
@@ -58,7 +74,7 @@ import torch.nn.functional as F
 
 
 def psnr(x: Tensor, x_hat: Tensor) -> float:
-    print(x.shape, x_hat.shape)
-    return -10 * math.log10(F.mse_loss(x, x_hat[:,:,:,0:360]).item())
+    #print(x.shape, x_hat.shape)
+    return -10 * math.log10(F.mse_loss(x, x_hat['x_hat']).item())
 
 print(psnr(x, x_hat))
---
+++ train_comp_ai.py
@@ -0,0 +1,294 @@
+import argparse
+import random
+import shutil
+import sys
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+from torch.utils.data import DataLoader
+from torchvision import transforms
+
+from compressai.datasets import ImageFolder
+from compressai.losses import RateDistortionLoss
+from compressai.optimizers import net_aux_optimizer
+from compressai.zoo import image_models
+
+
+class AverageMeter:
+    """Compute running average."""
+
+    def __init__(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+
+class CustomDataParallel(nn.DataParallel):
+    """Custom DataParallel to access the module methods."""
+
+    def __getattr__(self, key):
+        try:
+            return super().__getattr__(key)
+        except AttributeError:
+            return getattr(self.module, key)
+
+
+def configure_optimizers(net, args):
+    """Separate parameters for the main optimizer and the auxiliary optimizer.
+    Return two optimizers"""
+    conf = {
+        "net": {"type": "Adam", "lr": args.learning_rate},
+        "aux": {"type": "Adam", "lr": args.aux_learning_rate},
+    }
+    optimizer = net_aux_optimizer(net, conf)
+    return optimizer["net"], optimizer["aux"]
+
+
+def train_one_epoch(
+    model, criterion, train_dataloader, optimizer, aux_optimizer, epoch, clip_max_norm
+):
+    model.train()
+    device = next(model.parameters()).device
+
+    for i, d in enumerate(train_dataloader):
+        d = d.to(device)
+
+        optimizer.zero_grad()
+        aux_optimizer.zero_grad()
+
+        out_net = model(d)
+
+        out_criterion = criterion(out_net, d)
+        out_criterion["loss"].backward()
+        if clip_max_norm > 0:
+            torch.nn.utils.clip_grad_norm_(model.parameters(), clip_max_norm)
+        optimizer.step()
+
+        aux_loss = model.aux_loss()
+        aux_loss.backward()
+        aux_optimizer.step()
+
+        if i % 10 == 0:
+            print(
+                f"Train epoch {epoch}: ["
+                f"{i*len(d)}/{len(train_dataloader.dataset)}"
+                f" ({100. * i / len(train_dataloader):.0f}%)]"
+                f'\tLoss: {out_criterion["loss"].item():.3f} |'
+                f'\tMSE loss: {out_criterion["mse_loss"].item():.3f} |'
+                f'\tBpp loss: {out_criterion["bpp_loss"].item():.2f} |'
+                f"\tAux loss: {aux_loss.item():.2f}"
+            )
+
+
+def test_epoch(epoch, test_dataloader, model, criterion):
+    model.eval()
+    device = next(model.parameters()).device
+
+    loss = AverageMeter()
+    bpp_loss = AverageMeter()
+    mse_loss = AverageMeter()
+    aux_loss = AverageMeter()
+
+    with torch.no_grad():
+        for d in test_dataloader:
+            d = d.to(device)
+            out_net = model(d)
+            out_criterion = criterion(out_net, d)
+
+            aux_loss.update(model.aux_loss())
+            bpp_loss.update(out_criterion["bpp_loss"])
+            loss.update(out_criterion["loss"])
+            mse_loss.update(out_criterion["mse_loss"])
+
+    print(
+        f"Test epoch {epoch}: Average losses:"
+        f"\tLoss: {loss.avg:.3f} |"
+        f"\tMSE loss: {mse_loss.avg:.3f} |"
+        f"\tBpp loss: {bpp_loss.avg:.2f} |"
+        f"\tAux loss: {aux_loss.avg:.2f}\n"
+    )
+
+    return loss.avg
+
+
+def save_checkpoint(state, is_best, filename="checkpoint.pth.tar"):
+    torch.save(state, filename)
+    if is_best:
+        shutil.copyfile(filename, "checkpoint_best_loss.pth.tar")
+
+
+def parse_args(argv):
+    parser = argparse.ArgumentParser(description="Example training script.")
+    parser.add_argument(
+        "-m",
+        "--model",
+        default="bmshj2018-factorized",
+        choices=image_models.keys(),
+        help="Model architecture (default: %(default)s)",
+    )
+    parser.add_argument(
+        "-d", "--dataset", type=str, required=True, help="Training dataset"
+    )
+    parser.add_argument(
+        "-e",
+        "--epochs",
+        default=100,
+        type=int,
+        help="Number of epochs (default: %(default)s)",
+    )
+    parser.add_argument(
+        "-lr",
+        "--learning-rate",
+        default=1e-4,
+        type=float,
+        help="Learning rate (default: %(default)s)",
+    )
+    parser.add_argument(
+        "-n",
+        "--num-workers",
+        type=int,
+        default=4,
+        help="Dataloaders threads (default: %(default)s)",
+    )
+    parser.add_argument(
+        "--lambda",
+        dest="lmbda",
+        type=float,
+        default=1e-2,
+        help="Bit-rate distortion parameter (default: %(default)s)",
+    )
+    parser.add_argument(
+        "--batch-size", type=int, default=16, help="Batch size (default: %(default)s)"
+    )
+    parser.add_argument(
+        "--test-batch-size",
+        type=int,
+        default=64,
+        help="Test batch size (default: %(default)s)",
+    )
+    parser.add_argument(
+        "--aux-learning-rate",
+        type=float,
+        default=1e-3,
+        help="Auxiliary loss learning rate (default: %(default)s)",
+    )
+    parser.add_argument(
+        "--patch-size",
+        type=int,
+        nargs=2,
+        default=(256, 256),
+        help="Size of the patches to be cropped (default: %(default)s)",
+    )
+    parser.add_argument("--cuda", action="store_true", help="Use cuda")
+    parser.add_argument(
+        "--save", action="store_true", default=True, help="Save model to disk"
+    )
+    parser.add_argument("--seed", type=int, help="Set random seed for reproducibility")
+    parser.add_argument(
+        "--clip_max_norm",
+        default=1.0,
+        type=float,
+        help="gradient clipping max norm (default: %(default)s",
+    )
+    parser.add_argument("--checkpoint", type=str, help="Path to a checkpoint")
+    args = parser.parse_args(argv)
+    return args
+
+
+def main(argv):
+    args = parse_args(argv)
+
+    if args.seed is not None:
+        torch.manual_seed(args.seed)
+        random.seed(args.seed)
+
+    transform = transforms.Compose([
+        transforms.PILToTensor(),
+        transforms.Resize((64, 128)),
+        transforms.ConvertImageDtype(torch.float32),
+    ])
+
+    train_dataset = ImageFolder(args.dataset, split="train", transform=transform)
+    test_dataset = ImageFolder(args.dataset, split="test", transform=transform)
+
+    device = "cuda" if args.cuda and torch.cuda.is_available() else "cpu"
+
+    train_dataloader = DataLoader(
+        train_dataset,
+        batch_size=args.batch_size,
+        num_workers=args.num_workers,
+        shuffle=True,
+        pin_memory=(device == "cuda"),
+    )
+
+    test_dataloader = DataLoader(
+        test_dataset,
+        batch_size=args.test_batch_size,
+        num_workers=args.num_workers,
+        shuffle=False,
+        pin_memory=(device == "cuda"),
+    )
+
+    net = image_models[args.model](quality=3)
+    net = net.to(device)
+
+    if args.cuda and torch.cuda.device_count() > 1:
+        net = CustomDataParallel(net)
+
+    optimizer, aux_optimizer = configure_optimizers(net, args)
+    lr_scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, "min")
+    criterion = RateDistortionLoss(lmbda=args.lmbda)
+
+    last_epoch = 0
+    if args.checkpoint:  # load from previous checkpoint
+        print("Loading", args.checkpoint)
+        checkpoint = torch.load(args.checkpoint, map_location=device)
+        last_epoch = checkpoint["epoch"] + 1
+        net.load_state_dict(checkpoint["state_dict"])
+        optimizer.load_state_dict(checkpoint["optimizer"])
+        aux_optimizer.load_state_dict(checkpoint["aux_optimizer"])
+        lr_scheduler.load_state_dict(checkpoint["lr_scheduler"])
+
+    best_loss = float("inf")
+    for epoch in range(last_epoch, args.epochs):
+        print(f"Learning rate: {optimizer.param_groups[0]['lr']}")
+        train_one_epoch(
+            net,
+            criterion,
+            train_dataloader,
+            optimizer,
+            aux_optimizer,
+            epoch,
+            args.clip_max_norm,
+        )
+        loss = test_epoch(epoch, test_dataloader, net, criterion)
+        lr_scheduler.step(loss)
+
+        is_best = loss < best_loss
+        best_loss = min(loss, best_loss)
+
+        if args.save:
+            save_checkpoint(
+                {
+                    "epoch": epoch,
+                    "state_dict": net.state_dict(),
+                    "loss": loss,
+                    "optimizer": optimizer.state_dict(),
+                    "aux_optimizer": aux_optimizer.state_dict(),
+                    "lr_scheduler": lr_scheduler.state_dict(),
+                },
+                is_best,
+            )
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
